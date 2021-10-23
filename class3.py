c5ba9f971dffceca756acf5780efb299f0732f81
svenschilling <sven.schilling@gmail.com>
2021-10-23 16:56:37 +0200

    update

--- class3.py
+++ class3.py
@@ -1,8 +1,11 @@
 class Item:
+    
+    pay_rate = 0.8 # discount of 20%
+
     def __init__(self, name: str, price: float, quantity=0):
         # validations to received args
-        assert price >= 0
-        assert quantity >= 0 
+        assert price >= 0, f"Price {price} is not greater than or equal ro zero!"
+        assert quantity >= 0, f"Price {quantity} is not greater than or equal to zero!"
         
         # assign to self object
         self.itemName = name
@@ -18,4 +21,5 @@ item1 = Item("Phone",100,5)
 item2 = Item("Laptop",1000,3)
 print(f"{item2.itemName} costs {item2.itemPrice}")
 
-
+print(Item.__dict__)
+print(item1.__dict__)
