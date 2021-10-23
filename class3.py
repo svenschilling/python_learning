f2d8d81fb3bb34547da9b34f054eb2ac66461b70
svenschilling <sven.schilling@gmail.com>
2021-10-23 15:01:07 +0200

    update

--- class3.py
+++ class3.py
@@ -1,12 +1,21 @@
 class Item:
-    def __init__(self,name,price,quantity):
-        print(f"An Instance of {name} got created")
+    def __init__(self, name: str, price: float, quantity=0):
+        # validations to received args
+        assert price >= 0
+        assert quantity >= 0 
+        
+        # assign to self object
         self.itemName = name
         self.itemPrice = price
-        self.itemQuantity = quantity        
+        self.itemQuantity = quantity
+        
+
         
     def calculatePrice(self):
-        return item1.itemPrice * item1.itemQuantity
+        return self.itemPrice * self.itemQuantity
 
 item1 = Item("Phone",100,5)
-print(f"The Sum of {item1.itemName} costs {item1.calculatePrice()}")
+item2 = Item("Laptop",1000,3)
+print(f"{item2.itemName} costs {item2.itemPrice}")
+
+
